fc8875352319d132554b72ea9645a06d9a5d05f3
Jun Anthony <151007316+janthony25@users.noreply.github.com>
2024-02-10 08:24:18 +1300

    Add files via upload

---
+++ SQL Operators.sql
@@ -0,0 +1,32 @@
+Use AdventureWorks2017
+Go
+
+/***********************************************
+				SQL OPERATORS
+					=
+					>
+					<
+					>=
+					<=
+					<> not Equal
+
+***********************************************/
+
+
+
+/*
+Select * From Person.Person
+Where FirstName = 'Terri'
+*/
+
+Select * From Person.Person
+Where BusinessEntityID <> 5
+
+/*
+Select * From Person.Person
+Where BusinessEntityID > 20000
+*/
+/*
+Select * From Person.Person
+Where BusinessEntityID < 20
+*/
